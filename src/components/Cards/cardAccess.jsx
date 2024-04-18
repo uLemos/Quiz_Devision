949132c992d81b9ea5fd33f32c7783f299eb1abd
Fernando Santos <fernando.santos@zanchetta.com.br>
2024-04-17 21:08:42 -0300

    :memo: second screen, login page

--- src/components/Cards/cardAccess.jsx
+++ src/components/Cards/cardAccess.jsx
@@ -4,7 +4,20 @@ import '../../css/cardAccess.css';
 const CardAccess = () => {
   return (
     <div className="container">
-      
+      <form action="POST">
+        <h1>Entrar</h1>
+        <div className="container-form">
+          <input type="text" placeholder="Usuário" />
+          <input type="password" placeholder="Senha" />
+        </div>
+        <div className="container-subForm">
+          <div className="checkBox">
+            <input type="checkbox" />
+            <p>Lembre-se de mim</p>
+          </div>
+          <p>Esqueci minha senha</p>
+        </div>
+      </form>
     </div>    
   )
 }
--- src/components/screenInitial.jsx
+++ src/components/screenInitial.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Ellipses from './ellipses.jsx';
 import '../css/ellipses.css'
+import '../css/screenInitial.css'
 import CardAccess from './Cards/cardAccess.jsx';
 import { Link } from 'react-router-dom';
 
